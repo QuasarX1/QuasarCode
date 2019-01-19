630b66bdf5eae47c492ecaab47b981f7ad2004b3
Christopher Rowe <chris.rowe19@outlook.com>
2019-01-19 22:55:41 +0000

    Added more validators and a MultiItterator class. Added unit tests for respective elements.

--- UnitTests_Windows/UnitTest1.cs
+++ UnitTests_Windows/UnitTest1.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using QuasarCode;
 using static QuasarCode.Library.IO.Text.Console;
 using static QuasarCode.Library.Tools.Validators;
+using QuasarCode.Library.Tools;
 
 namespace UnitTests_Windows
 {
@@ -19,6 +21,7 @@ namespace UnitTests_Windows
             Print("Hi", "Chris. This is the ", 3, "rd test.");
         }
 
+        // DO NOT RUN - requires input
         [TestMethod]
         public void Input_Test()
         {
@@ -38,6 +41,7 @@ namespace UnitTests_Windows
             Input<int>("Numerical input wanted:", new Func<string, int>(convert), new Func<object, bool>(IsInt), "The input wasn't an integer.");
         }
 
+        // DO NOT RUN - requires input
         [TestMethod]
         public void Option_Test()
         {
@@ -97,6 +101,69 @@ namespace UnitTests_Windows
             Assert.AreEqual(IsString("A string"), true);
             Assert.AreEqual(IsString('a'), true);
             Assert.AreEqual(IsString(5), true);
+        }
+
+        [TestMethod]
+        public void Range_Test()
+        {
+            Assert.AreEqual(InRange(50, 0, 100), true);
+            Assert.AreEqual(InRange(-1, 0, 100), false);
+            Assert.AreEqual(InRange(0, 0, 100), true);
+            Assert.AreEqual(InRange(1, 0, 100), true);
+            Assert.AreEqual(InRange(99, 0, 100), true);
+            Assert.AreEqual(InRange(100, 0, 100), false);
+            Assert.AreEqual(InRange(101, 0, 100), false);
+
+
+            Assert.AreEqual(InRange(50, 0, 100, inside: false), false);
+            Assert.AreEqual(InRange(-1, 0, 100, inside: false), true);
+            Assert.AreEqual(InRange(0, 0, 100, inside: false), false);
+            Assert.AreEqual(InRange(1, 0, 100, inside: false), false);
+            Assert.AreEqual(InRange(99, 0, 100, inside: false), false);
+            Assert.AreEqual(InRange(100, 0, 100, inside: false), true);
+            Assert.AreEqual(InRange(101, 0, 100, inside: false), true);
+        }
+
+        [TestMethod]
+        public void Even_Test()
+        {
+            Assert.AreEqual(IsEven(0), true);
+            Assert.AreEqual(IsEven(1), false);
+            Assert.AreEqual(IsEven(2), true);
+
+            Assert.AreEqual(IsEven(0.0), true);
+            Assert.AreEqual(IsEven(1.0), false);
+            Assert.AreEqual(IsEven(2.0), true);
+
+            Assert.AreEqual(IsEven(0.5), true);
+            Assert.AreEqual(IsEven(1.4), false);
+            Assert.AreEqual(IsEven(1.5), true);// Conversion to integer rounds to nearest int
+            Assert.AreEqual(IsEven(2.5), true);
+        }
+
+        [TestMethod]
+        public void MultiItterator_Test()
+        {
+            foreach (Tuple<int, object[]> items in new MultiItterator(new object[] { 1, 2, 3 }, new string[] { "a", "b", "c" }, new object[] { false, true, false }))
+            {
+                Assert.AreEqual(items.Item1, (int)items.Item2[0] - 1);
+                Assert.AreEqual(IsEven(items.Item2[0]), (bool)items.Item2[2]);
+            }
+
+
+            int[] result1 = new int[] { 3, 2, 4 };
+            int[] result2 = new int[] { 1, 5, 3 };
+            int[] result3 = new int[] { 4, 2, 5 };
+
+            int[] averages = new int[] { 0, 0, 0 };
+            foreach (Tuple<int, int[]> items in new MultiItterator<int>(result1, result2, result3))
+            {
+                averages[items.Item1] = (items.Item2[0] + items.Item2[1] + items.Item2[2]) / 3;
+            }
+            
+            Assert.AreEqual(averages[0], 2);// Truncates float
+            Assert.AreEqual(averages[1], 3);
+            Assert.AreEqual(averages[2], 4);
         }
     }
 }
