50e7f7fb3bdaccf09a76dfca38b837bc7640d2ff
Christopher Rowe <chris.rowe19@outlook.com>
2019-02-04 21:39:44 +0000

    Added classes for each of the coordinate systems and respective coordinate instances.

---
+++ QuasarCode_Library/Maths/Coordinates/ND/Cartesian_ND_Coordinate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using QuasarCode.Library.Maths.Coordinates.Systems;
+using QuasarCode.Library.Maths.Coordinates.Systems.ND;
+
+
+namespace QuasarCode.Library.Maths.Coordinates.ND
+{
+    public class Cartesian_ND_Coordinate : ICoordinate<Cartesian_ND>
+    {
+        public ICoordinateSystem<Cartesian_ND> System { get; }
+
+        public double[] Ordinates { get; }
+
+        public int Dimentions { get { return System.Dimentions; } }
+
+        public Cartesian_ND_Coordinate(ICoordinateSystem<Cartesian_ND> coordinateSystem, params double[] ordinates)
+        {
+            System = coordinateSystem;
+
+            Ordinates = ordinates;
+        }
+
+        public Vector<Cartesian_ND> GetVector()
+        {
+
+        }
+    }
+}
---
+++ QuasarCode_Library/Maths/Coordinates/Systems/ND/Cartesian_ND.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuasarCode.Library.Maths.Coordinates.Systems.ND
+{
+    public class Cartesian_ND : ICoordinateSystem<Cartesian_ND>
+    {
+        public Dictionary<string, IGeneralUnit> Axes { get; }
+
+        public int Dimentions { get; }
+
+
+        public Cartesian_ND(params Units[] axisUnits)
+        {
+            Axes = new Dictionary<string, IGeneralUnit>();
+
+            for (int i = 0; i < axisUnits.Length; i++)
+            {
+                Axes.Add(i.ToString(), (Unit)axisUnits[i]);
+            }
+
+            Dimentions = Axes.Count;
+        }
+
+        public Cartesian_ND(params IGeneralUnit[] axisUnits)
+        {
+            Axes = new Dictionary<string, IGeneralUnit>();
+
+            for (int i = 0; i < axisUnits.Length; i++)
+            {
+                Axes.Add(i.ToString(), axisUnits[i]);
+            }
+
+            Dimentions = Axes.Count;
+        }
+
+
+        public event Func<ICoordinateSystem<Cartesian_ND>, ICoordinate<Cartesian_ND>[]> ReportPosition;
+    }
+}
