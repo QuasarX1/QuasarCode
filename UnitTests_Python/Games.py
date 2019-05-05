77e163813b146494cef59c39d694db6568dbcd6e
Christopher Rowe <chris.rowe19@outlook.com>
2019-05-05 19:09:11 +0100

    Bugfixes imports and added tests for numbered dice.

--- UnitTests_Python/Games.py
+++ UnitTests_Python/Games.py
@@ -1,6 +1,6 @@
 import unittest
 from QuasarCode.Games.Spinners import Spinner, ISpinner
-from QuasarCode.Games.Dice import NDice, IDice
+from QuasarCode.Games.Dice import NDice, IDice, Dice6, Dice8, Dice12
 
 class Test_Games(unittest.TestCase):
     def test_Spinners(self):
@@ -14,9 +14,20 @@ class Test_Games(unittest.TestCase):
 
         for i in range(100):
             print(dice.Roll())
+        print()
 
-    self.assertEqual(len(NDice.MultipleDice([6, 6])), 2, "Wrong number of dice produced by MultipleDice.")
-    self.assertEqual(len(NDice.MultipleDice([6], noOfDice = 2)), 2, "Wrong number of dice produced by MultipleDice.")
+        self.assertEqual(len(NDice.MultipleDice([6, 6])), 2, "Wrong number of dice produced by MultipleDice.")
+        self.assertEqual(len(NDice.MultipleDice([6], noOfDice = 2)), 2, "Wrong number of dice produced by MultipleDice.")
+
+        dice6 = Dice6()
+        dice8 = Dice8()
+        dice12 = Dice12()
+
+        print(dice6.Roll())
+
+        print(dice8.Roll())
+
+        print(dice12.Roll())
 
 if __name__ == '__main__':
     unittest.main()
