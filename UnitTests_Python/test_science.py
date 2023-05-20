167b2c678b41c26f845576841a71451409eb9279
Christopher Rowe <chris.rowe19@outlook.com>
2023-05-20 01:31:17 +0100

    Adds code for generating matplotlib polygons.
    Also hides legacy science plotting code (still present) and updates the version number to 0.8.0

--- UnitTests_Python/test_science.py
+++ UnitTests_Python/test_science.py
@@ -1,10 +1,13 @@
 import pytest
 from decimal import Decimal
 import numpy as np
+from matplotlib import pyplot as plt
 
 ##from QuasarCode.Science.graph import Figure
 ##from QuasarCode.Science.data import errorString, sigFig
-from QuasarCode.Science import Figure, error_string, sig_fig
+#from QuasarCode.Science import Figure, error_string, sig_fig
+
+from QuasarCode.Science import error_string, sig_fig, polygon_y_error_region, polygon_x_error_region
 
 class TestClass_Science(object):
     def test_sig_fig(self):
@@ -33,26 +36,41 @@ class TestClass_Science(object):
     def test_consistancyTest(self):
         pass#TODO:
 
-    def test_GraphAndErrorString(self):
-        x = np.array([1.3, 2.1, 2.8, 3.9, 5.2])
-        y = np.array([3.4, 4.6, 7.2, 8.5, 11.2])
+ #   def test_GraphAndErrorString(self):
+ #       x = np.array([1.3, 2.1, 2.8, 3.9, 5.2])
+ #       y = np.array([3.4, 4.6, 7.2, 8.5, 11.2])
+ #
+ #       dx = np.array([1, 1, 1, 1, 1])
+ #       dy = np.array([1, 1, 1, 1, 1])
+ #
+ #       testGraph = Figure(x, y, dx, dy, size = None, title = "A test graph", xLabel = "X-Axis", yLabel = "Y-Axis", saveName = "testingFigure.png")
+ #       testGraph.createFitLine()
+ #       testGraph.plot()
+ #       testGraph.save()
+ #       #testGraph.show()# TODO: window hangs
+ #
+ #       m = testGraph.fit.fitVariables["m"]
+ #       c = testGraph.fit.fitVariables["c"]
+ #
+ #       assert "2.0435922040733323" == str(m[0]), "The value calculated for the gradient does not match the expected value."
+ #       assert "0.7481821730455976" == str(m[1]), "The error calculated for the gradient does not match the expected value."
+ #       assert "0.7266078556762237" == str(c[0]), "The value calculated for the intercept does not match the expected value."
+ #       assert "2.505349774682801" == str(c[1]), "The error calculated for the intercept does not match the expected value."
+ #
+ #       assert "2.0435922040733323 \u00B1 0.7" == error_string(m[0], m[1]), "The normalised value and error calculated for the gradient does not match the expected value."
+ #       assert "0.7266078556762237 \u00B1 3.0" == error_string(c[0], c[1]), "The normalised value and error calculated for the intercept does not match the expected value."
 
-        dx = np.array([1, 1, 1, 1, 1])
-        dy = np.array([1, 1, 1, 1, 1])
+    def test_plotting(self):
+        plt.figure()
+        polygon_y_error_region([0, 1, 2, 3, 4, 5], [1, 3, 1, 3, 1, 3], [-3, -1, -3, -1, -3, -1], "default", color = "red", alpha = 0.5)
+        polygon_y_error_region([0, 1, 2, 3, 4, 5], [3, 1, 3, 1, 3, 1], [-1, -3, -1, -3, -1, -3], plt.gca(), color = "yellow", alpha = 0.5)
+        plt.xlim((0, 5))
+        plt.ylim((-3, 3))
+        plt.show()
 
-        testGraph = Figure(x, y, dx, dy, size = None, title = "A test graph", xLabel = "X-Axis", yLabel = "Y-Axis", saveName = "testingFigure.png")
-        testGraph.createFitLine()
-        testGraph.plot()
-        testGraph.save()
-        #testGraph.show()# TODO: window hangs
-
-        m = testGraph.fit.fitVariables["m"]
-        c = testGraph.fit.fitVariables["c"]
-
-        assert "2.0435922040733323" == str(m[0]), "The value calculated for the gradient does not match the expected value."
-        assert "0.7481821730455976" == str(m[1]), "The error calculated for the gradient does not match the expected value."
-        assert "0.7266078556762237" == str(c[0]), "The value calculated for the intercept does not match the expected value."
-        assert "2.505349774682801" == str(c[1]), "The error calculated for the intercept does not match the expected value."
-
-        assert "2.0435922040733323 \u00B1 0.7" == error_string(m[0], m[1]), "The normalised value and error calculated for the gradient does not match the expected value."
-        assert "0.7266078556762237 \u00B1 3.0" == error_string(c[0], c[1]), "The normalised value and error calculated for the intercept does not match the expected value."
+        plt.figure()
+        polygon_x_error_region([-3, -1, -3, -1, -3, -1], [1, 3, 1, 3, 1, 3], [0, 1, 2, 3, 4, 5], "default", color = "red", alpha = 0.5)
+        polygon_x_error_region([-1, -3, -1, -3, -1, -3], [3, 1, 3, 1, 3, 1], [0, 1, 2, 3, 4, 5], plt.gca(), color = "yellow", alpha = 0.5)
+        plt.xlim((0, 5))
+        plt.ylim((-3, 3))
+        plt.show()
