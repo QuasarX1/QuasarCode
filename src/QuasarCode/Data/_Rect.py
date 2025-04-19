c459070047b278bbba90f363eb40a68a4dc8e572
Christopher J. R. Rowe <chris.rowe19@outlook.com>
2025-04-19 14:54:49 +0100

    Adds a Rect class.

---
+++ src/QuasarCode/Data/_Rect.py
@@ -0,0 +1,138 @@
+class Rect:
+    """
+    A 2D rectangle.
+
+    Parameters:
+        float x_min:
+            X-coordinate of the bottom-left corner.
+        float x_max:
+            X-coordinate of the top-right corner.
+        float y_min:
+            Y-coordinate of the bottom-left corner.
+        float y_max:
+            Y-coordinate of the top-right corner.
+
+    Use the following static methods to create an instance:
+        - create_from_size(x, y, width, height)
+        - create_from_center(x, y, width, height)
+        - create_from_limits(x_min, x_max, y_min, y_max)
+    """
+    def __init__(self, x_min: float, x_max: float, y_min: float, y_max: float):
+        self.__x_min = x_min
+        self.__x_max = x_max
+        self.__y_min = y_min
+        self.__y_max = y_max
+
+    @staticmethod
+    def create_from_size(x: float, y: float, width: float, height: float) -> "Rect":
+        """
+        Creates a Rect instance using the specified position of the bottom-left corner and size.
+
+        Parameters:
+            float x:
+                The x-coordinate of the bottom-left corner of the rectangle.
+            float y:
+                The y-coordinate of the bottom-left corner of the rectangle.
+            float width:
+                The width of the rectangle.
+            float height:
+                The height of the rectangle.
+
+        Returns:
+            Rect -> A new Rect instance defined by the given position and size.
+        """
+        return Rect(x, x + width, y, y + height)
+
+    @staticmethod
+    def create_from_center(x: float, y: float, width: float, height: float) -> "Rect":
+        """
+        Create a Rect instance using the center coordinates and dimensions.
+
+        Parameters:
+            float x:
+                The x-coordinate of the center of the rectangle.
+            float y:
+                The y-coordinate of the center of the rectangle.
+            float width:
+                The width of the rectangle.
+            float height:
+                The height of the rectangle.
+
+        Returns:
+            Rect -> Instance centred on (x, y) with the specified width and height.
+        """
+        return Rect(x - width / 2, x + width / 2, y - height / 2, y + height / 2)
+
+    @staticmethod
+    def create_from_limits(x_min: float, x_max: float, y_min: float, y_max: float) -> "Rect":
+        """
+        Create a Rect instance from the specified boundary limits.
+
+        Parameters:
+            float x_min:
+                The minimum x-coordinate of the rectangle.
+            float x_max:
+                The maximum x-coordinate of the rectangle.
+            float y_min:
+                The minimum y-coordinate of the rectangle.
+            float y_max:
+                The maximum y-coordinate of the rectangle.
+
+        Returns:
+            Rect -> A new instance of the Rect class defined by the given limits.
+        """
+        return Rect(x_min, x_max, y_min, y_max)
+
+    @property
+    def extent(self) -> tuple[float, float, float, float]:
+        """
+        The limits of the rectangle in the form (x_min, x_max, y_min, y_max).
+        This is the same as the constructor parameters and is implemented for consistency.
+        This corresponds to the format of a matplotlib 'extent' parameter.
+        """
+        return (self.__x_min, self.__x_max, self.__y_min, self.__y_max)
+
+    @property
+    def width(self) -> float:
+        """
+        The width of the rectangle.
+
+        Returns:
+            float -> The width of the rectangle.
+        """
+        return self.__x_max - self.__x_min
+
+    @property
+    def height(self) -> float:
+        """
+        The height of the rectangle.
+        
+        Returns:
+            float -> The height of the rectangle.
+        """
+        return self.__y_max - self.__y_min
+
+    @property
+    def centre(self) -> tuple[float, float]:
+        """
+        The coordinates of the center of the rectangle.
+
+        Returns:
+            tuple[float, float] -> (x, y)
+        """
+        return ((self.__x_max + self.__x_min) / 2, (self.__y_max + self.__y_min) / 2)
+    
+    def __copy__(self) -> "Rect":
+        """
+        Create a shallow copy of the rectangle.
+
+        Returns:
+            Rect -> A new instance of the Rect class with the same properties.
+        """
+        return Rect(self.__x_min, self.__x_max, self.__y_min, self.__y_max)
+    
+    def __str__(self) -> str:
+        return f"Rect(x: {self.__x_min} -> {self.__x_max}, y: {self.__y_min} -> {self.__y_max})"
+    
+    def __repr__(self) -> str:
+        return self.__str__()
--- src/QuasarCode/Data/__init__.py
+++ src/QuasarCode/Data/__init__.py
@@ -1,3 +1,4 @@
 from ._VersionInformation import VersionInfomation
 from ._PersonInfomation import PersonInfomation
 from ._AuthorInfomation import AuthorInfomation
+from ._Rect import Rect
