e968a55a9b8d7444ce59ab5f228b4c1606e81eb1
Christopher J. R. Rowe <chris.rowe19@outlook.com>
2025-04-20 16:46:38 +0100

    Adds creation of cached scatter plot elements from matplotlib objects.

--- src/QuasarCode/Plotting/_CachedPlotElements.py
+++ src/QuasarCode/Plotting/_CachedPlotElements.py
@@ -1,4 +1,4 @@
-from typing import Any, Literal, TypeVar
+from typing import Any, Literal, Sequence, TypeVar
 from abc import abstractmethod
 
 from matplotlib.axes import Axes
@@ -7,7 +7,8 @@ from matplotlib.colorbar import Colorbar
 from matplotlib.colorizer import ColorizingArtist
 from matplotlib.colors import Colormap
 from matplotlib.figure import Figure
-from matplotlib.typing import ColorType
+from matplotlib.pylab import ArrayLike
+from matplotlib.typing import ColorType, MarkerType
 import numpy as np
 from matplotlib.collections import PathCollection, PolyCollection
 from matplotlib.container import ErrorbarContainer
@@ -46,13 +47,14 @@ class CachedPlotLine(CachedPlotElement[Line2D]):
     x = AutoProperty_NonNullable[np.ndarray[tuple[int], np.dtype[np.floating]]]()
     y = AutoProperty_NonNullable[np.ndarray[tuple[int], np.dtype[np.floating]]]()
     label = AutoProperty[str]()
-    colour = AutoProperty[str]()
+    colour = AutoProperty[ColorType]()
     linestyle = AutoProperty[str]()
     linewidth = AutoProperty[float]()
+    alpha = AutoProperty_NonNullable[float](default_value = 1.0)
     def __init__(self, **kwargs):
-        super().__init__("x", "y", "label", "colour", "linestyle", **kwargs)
+        super().__init__("x", "y", "label", "colour", "linestyle", "linewidth", "alpha", **kwargs)
     def render(self, figure: Figure, axis: Axes, *args: Any, **kwargs: Any) -> None:
-        self._result = axis.plot(self.x, self.y, label = self.label, colour = self.colour, linestyle = self.linestyle, linewidth = self.linewidth)[0]
+        self._result = axis.plot(self.x, self.y, label = self.label, colour = self.colour, linestyle = self.linestyle, linewidth = self.linewidth, alpha = self.alpha)[0]
     @staticmethod
     def from_line(line: Line2D) -> "CachedPlotLine":
         coords: np.ndarray[tuple[int, int], np.dtype[np.floating]] = line.get_xydata()
@@ -63,17 +65,37 @@ class CachedPlotLine(CachedPlotElement[Line2D]):
             colour = line.get_color(),
             linestyle = line.get_linestyle(),
             linewidth = line.get_linewidth(),
+            alpha = line.get_alpha(),
             _result = line
         )
 
 class CachedPlotScatter(CachedPlotElement[PathCollection]):
     x = AutoProperty_NonNullable[np.ndarray[tuple[int], np.dtype[np.floating]]]()
     y = AutoProperty_NonNullable[np.ndarray[tuple[int], np.dtype[np.floating]]]()
+    label = AutoProperty[str]()
+    colour = AutoProperty[ColorType|Sequence[ColorType]]()
     colourmap = AutoProperty[str|Colormap]()
+    marker = AutoProperty[MarkerType]()
+    size = AutoProperty[float|ArrayLike]()
+    alpha = AutoProperty[float|ArrayLike]()
     def __init__(self, **kwargs):
-        super().__init__("x", "y", "colourmap", **kwargs)
+        super().__init__("x", "y", "label", "colour", "colourmap", "marker", "size", "alpha", **kwargs)
     def render(self, figure: Figure, axis: Axes, *args: Any, **kwargs: Any):
-        self._result = axis.scatter(self.x, self.y, cmap = self.colourmap)
+        self._result = axis.scatter(self.x, self.y, c = self.colour, cmap = self.colourmap, marker = self.marker, s = self.size, alpha = self.alpha, label = self.label, **kwargs)
+    @staticmethod
+    def from_points(points: PathCollection) -> "CachedPlotScatter":
+        coords: np.ndarray[tuple[int, int], np.dtype[np.floating]] = points.get_offsets()
+        return CachedPlotScatter(
+            x = coords[:, 0],
+            y = coords[:, 1],
+            label = points.get_label(),
+            colour = points.get_facecolor(),
+            colourmap = points.get_cmap(),
+            marker = points.get_paths()[0],
+            size = points.get_sizes(),
+            alpha = points.get_alpha(),
+            _result = points
+        )
 
 class CachedPlotErrorbar(CachedPlotElement[ErrorbarContainer]):
     x = AutoProperty_NonNullable[np.ndarray[tuple[int], np.dtype[np.floating]]]()
