4afad013c947aecedd16ca1c101079574d25af7f
Christopher J. R. Rowe <chris.rowe19@outlook.com>
2025-06-04 14:07:46 +0100

    Adds methods to the cacheable figure to allow sizing of rows and columns.

--- src/QuasarCode/Plotting/_CachedFigureGrid.py
+++ src/QuasarCode/Plotting/_CachedFigureGrid.py
@@ -2,6 +2,7 @@ from typing import Any, Literal
 
 from matplotlib.figure import Figure
 from matplotlib.axes import Axes
+import numpy as np
 
 from ..Data._Rect import Rect
 from ..Tools._Struct import CacheableStruct
@@ -16,11 +17,32 @@ class CachedFigureGrid(CacheableStruct):
     columns = AutoProperty_NonNullable[int](default_value = 1)
     figure_size = AutoProperty_NonNullable[tuple[int, int]](default_value = (6, 8))
     layout = AutoProperty[Literal["constrained", "compressed", "tight"]](allow_uninitialised = True)
-    relative_widths = AutoProperty[list[float]]()
     relative_heights = AutoProperty[list[float]]()
+    relative_widths = AutoProperty[list[float]]()
     vertical_spacing = AutoProperty_NonNullable[float](default_value = 0)
     horizontal_spacing = AutoProperty_NonNullable[float](default_value = 0)
     plots = AutoProperty_NonNullable[dict[str, CachedPlot]]()
+
+    @property
+    def physical_rect(self) -> Rect:
+        return Rect.create_from_size(
+            x = 0,
+            y = 0,
+            width = self.figure_size[0],
+            height = self.figure_size[1]
+        )
+
+    @property
+    def physical_heights(self) -> list[float]:
+        total_height = self.figure_size[1]
+        total_relative_height = sum(self.relative_heights) if self.relative_heights else self.rows
+        return [total_height * (row_height / total_relative_height) for row_height in (self.relative_heights if self.relative_heights else [1] * self.rows)]
+    
+    @property
+    def physical_widths(self) -> list[float]:
+        total_width = self.figure_size[0]
+        total_relative_width = sum(self.relative_widths) if self.relative_widths else self.columns
+        return [total_width * (column_width / total_relative_width) for column_width in (self.relative_widths if self.relative_widths else [1] * self.columns)]
 
     def __init__(self, **kwargs):
         self.__figure: Figure|None = None
@@ -47,29 +69,94 @@ class CachedFigureGrid(CacheableStruct):
 
         self.plots = {}
 
-    def add_row(self, number = 1, insert_at_index: int = -1) -> None:
+    def add_row(self, number = 1, insert_at_index: int = -1, height: float = 1.0, physical_height: bool = False, expand_figure: bool = False) -> None:
         if number < 1:
             raise ValueError("Number of rows to add must be at least 1.")
         if self.__locked:
             raise RuntimeError("Cannot add rows after the figure has been created. Call `clear_axes` first.")
+        if physical_height and not expand_figure:
+            raise ValueError("Cannot add rows with physical height without expanding the figure. Set `expand_figure` to True.")
+        if self.relative_heights is None:
+            self.relative_heights = [1.0] * self.rows
+        if physical_height:
+            initial_physical_height = self.figure_size[1]
+            if expand_figure:
+                self.figure_size = (self.figure_size[0], self.figure_size[1] + height * number)
+            height *= (sum(self.relative_heights) if self.relative_heights else self.rows) / initial_physical_height
+        elif expand_figure:
+            self.figure_size = (self.figure_size[0], self.figure_size[1] + (height * (self.figure_size[1] / sum(self.relative_heights))) * number)
         for _ in range(number):
             self.mosaic.insert(insert_at_index, ["." for _ in range(self.columns)])
+            self.relative_heights.insert(insert_at_index, height)
         self.rows += number
 
-    def add_column(self, number = 1, insert_at_index: int = -1) -> None:
+    def add_column(self, number = 1, insert_at_index: int = -1, width: float = 1.0, physical_width: bool = False, expand_figure: bool = False) -> None:
         if number < 1:
             raise ValueError("Number of columns to add must be at least 1.")
         if self.__locked:
             raise RuntimeError("Cannot add columns after the figure has been created. Call `clear_axes` first.")
+        if physical_width and not expand_figure:
+            raise ValueError("Cannot add columns with physical width without expanding the figure. Set `expand_figure` to True.")
+        if self.relative_widths is None:
+            self.relative_widths = [1.0] * self.columns
+        if physical_width:
+            initial_physical_width = self.figure_size[0]
+            if expand_figure:
+                self.figure_size = (self.figure_size[0] + width * number, self.figure_size[1])
+            width *= (sum(self.relative_widths) if self.relative_widths else self.columns) / initial_physical_width
+        elif expand_figure:
+            self.figure_size = (self.figure_size[0] + (width * (self.figure_size[0] / sum(self.relative_widths))) * number, self.figure_size[1])
         for row in self.mosaic:
             for _ in range(number):
                 row.insert(insert_at_index, ".")
         self.columns += number
+        for _ in range(number):
+            self.relative_widths.insert(insert_at_index, width)
 
-    def set_empty(self, row: int|list[int], column: int|list[int]) -> None:
-        self.set_plot(".", row, column)
+    def resize_row(self, row: int|list[int], height: float, physical_height: bool = False, resize_figure: bool = False) -> None:
+        if self.__locked:
+            raise RuntimeError("Cannot resize rows after the figure has been created. Call `clear_axes` first.")
+        if isinstance(row, int):
+            row = [row]
+        if self.relative_heights is None:
+            self.relative_heights = [1.0] * self.rows
+        initial_physical_figure_height = self.figure_size[1]
+        initial_relative_figure_height = sum(self.relative_heights)
+        new_relative_height = height * (self.figure_size[1] / initial_physical_figure_height) if physical_height else height
+        old_relative_heights = []
+        for i in row:
+            if i >= self.rows or i < -self.rows:
+                raise IndexError(f"Row index {i} is out of bounds for the number of rows ({self.rows}).")
+            old_relative_heights.append(self.relative_heights[i])
+            self.relative_heights[i] = new_relative_height
+        if resize_figure:
+            delta_physical_height = (new_relative_height * len(row) - sum(old_relative_heights)) * (initial_physical_figure_height / initial_relative_figure_height)
+            self.figure_size = (self.figure_size[0], self.figure_size[1] + delta_physical_height)
 
-    def set_plot(self, name: str, row: int|list[int] = 0, column: int|list[int] = 0) -> None:
+    def resize_column(self, column: int|list[int], width: float, physical_width: bool = False, resize_figure: bool = False) -> None:
+        if self.__locked:
+            raise RuntimeError("Cannot resize columns after the figure has been created. Call `clear_axes` first.")
+        if isinstance(column, int):
+            column = [column]
+        if self.relative_widths is None:
+            self.relative_widths = [1.0] * self.columns
+        initial_physical_figure_width = self.figure_size[0]
+        initial_relative_figure_width = sum(self.relative_widths)
+        new_relative_width = width * (self.figure_size[0] / initial_physical_figure_width) if physical_width else width
+        old_relative_widths = []
+        for j in column:
+            if j >= self.columns or j < -self.columns:
+                raise IndexError(f"Column index {j} is out of bounds for the number of columns ({self.columns}).")
+            old_relative_widths.append(self.relative_widths[j])
+            self.relative_widths[j] = new_relative_width
+        if resize_figure:
+            delta_physical_width = (new_relative_width * len(column) - sum(old_relative_widths)) * (initial_physical_figure_width / initial_relative_figure_width)
+            self.figure_size = (self.figure_size[0] + delta_physical_width, self.figure_size[1])
+
+    def assign_empty(self, row: int|list[int], column: int|list[int]) -> None:
+        self.assign_plot(".", row, column)
+
+    def assign_plot(self, name: str, row: int|list[int] = 0, column: int|list[int] = 0) -> None:
         if isinstance(row, int):
             row = [row]
         if isinstance(column, int):
@@ -82,15 +169,39 @@ class CachedFigureGrid(CacheableStruct):
                     raise IndexError(f"Column index {j} is out of bounds for the number of columns ({self.columns}).")
                 self.mosaic[i][j] = name
 
-    def assign_plot(self, name: str, plot: CachedPlot) -> None:
+    def set_plot(self, name: str, plot: CachedPlot) -> None:
         if not any([name in row for row in self.mosaic]):
-            raise KeyError(f"No plot with the name \"{name}\" exists in the mosaic. Add the plot first with `set_plot`.")
+            raise KeyError(f"No plot with the name \"{name}\" exists in the mosaic. Add the plot first with `assign_plot`.")
         self.plots[name] = plot
 
-    def unassign_plot(self, name: str) -> None:
+    def remove_plot(self, name: str) -> None:
         try:
             self.plots.pop(name)
         except KeyError: pass
+
+    def get_axis_rect_relative(self, name: str) -> Rect:
+        if name == ".":
+            raise ValueError("Plot tag \".\" is reserved for empty space. To use this space, assign a name first.")
+        row_indexes, column_indexes = np.where(np.array(self.mosaic, dtype = str) == name)
+        heights = (self.relative_heights if self.relative_heights else [1] * self.rows   )
+        widths  = (self.relative_widths  if self.relative_widths  else [1] * self.columns)
+        return Rect.create_from_size(
+            x = sum(widths[:column_indexes[0]]),
+            y = sum(heights[:row_indexes[0]]),
+            width  = sum(widths[column_indexes[0] : column_indexes[-1] + 1]),
+            height = sum(heights[row_indexes[0] : row_indexes[-1] + 1])
+        )
+
+    def get_axis_rect_physical(self, name: str) -> Rect:
+        relative = self.get_axis_rect_relative(name)
+        total_relative_height = sum(self.relative_heights) if self.relative_heights else self.rows
+        total_relative_width = sum(self.relative_widths) if self.relative_widths else self.columns
+        return Rect.create_from_size(
+            x = (relative.x / total_relative_width) * self.figure_size[0],
+            y = (relative.y / total_relative_height) * self.figure_size[1],
+            width = (relative.width / total_relative_width) * self.figure_size[0],
+            height = (relative.height / total_relative_height) * self.figure_size[1]
+        )
 
     def clear_axes(self) -> None:
         if self.__locked:
@@ -127,7 +238,7 @@ class CachedFigureGrid(CacheableStruct):
     
     def get_axis(self, plot_tag: str) -> Axes:
         if plot_tag == ".":
-            raise ValueError("Plot tag \".\" is reserved for empty plots. To use these axes, assign them a name first.")
+            raise ValueError("Plot tag \".\" is reserved for empty space. To use these axes, assign them a name first.")
         if not self.__locked:
             raise RuntimeError("Axes have not been created yet. Call `make_figure_and_axes` first.")
         if plot_tag not in self.__axes:
